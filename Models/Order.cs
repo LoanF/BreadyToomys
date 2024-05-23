63cf22c8bf700c085d360662d86bd2a9c2897867
Loan François <loanfrancois1@gmail.com>
2024-05-23 15:05:07 +0200

    Ajoutez des fichiers projet.

---
+++ Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreadyToomys.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int OrderNumber { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreadyToomys.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+        public string Picture { get; set; }
+    }
+}
